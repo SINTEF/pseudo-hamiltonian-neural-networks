6f88f989bcadd8c620b8a9cec46bb54013102bed
solvee <solve.eidnes@sintef.no>
2023-08-24 12:03:57 +0200

    Updated docs conf file

--- phlearn/docs/source/conf.py
+++ phlearn/docs/source/conf.py
@@ -7,6 +7,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+#
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
